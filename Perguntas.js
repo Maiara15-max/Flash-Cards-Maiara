2ad318adfff0c2a2fc23b89e86ea846d5022b29a
maiara_da_fonseca <maiara.fonseca.ferreira@escola.pr.gov.br>
2025-09-09 08:45:10 -0300

    09/09

---
+++ Perguntas.js
@@ -0,0 +1,63 @@
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'programacao',
+    1) Quantos anos demorou para construir a torre de Paris ?,
+        'R: 2 anos, 2 meses e 5 dias.'
+
+criaCartao(
+    2)De onde é a invenção do chuveiro elétrico ?,
+        'R:Brasil'
+
+criaCartao(
+            3)Quantas casas decimais tem o número pi ?,
+                'R:Infinitas'
+
+criaCartao(
+    4)Qual a montanha mais alta do Brasil ?,
+        'R:Pico da Neblina'
+
+criaCartao(
+            5)Qual a velocidade da luz ?,
+                'R: 299 792 458 metros por segundo (m/s)'
+
+criaCartao(
+    6)Quem é o autor de “O Príncipe”?,
+'R:Maquiavel'
+
+criaCartao(
+    7)Quais as maiores pandemias da história ?,
+        'R:Peste negra e covid-19'
+
+criaCartao(
+    8)Quantos braços tem um polvo ?,
+        'R:8'
+
+criaCartao(
+    9)Quanto tempo o vidro demora para se decompor ?,
+        'R:tempo indeterminado'
+
+criaCartao(
+    10)Quem inventou a lâmpada ?,
+        'R: Thomas Edison'
+
+criaCartao(
+    11)Quem foi a primeira pessoa a viajar no Espaço ?,
+        'R:Yuri Gagarin'
+
+criaCartao(
+    12)A que temperatura a água ferve ?,
+        'R:100 ºC'
+
+    
+
+
+)
+
+
+
+
