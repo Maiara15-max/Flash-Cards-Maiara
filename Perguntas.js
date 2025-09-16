b713f8e5209c8c9f8314a9bad9c0a3a15dbb480e
maiara_da_fonseca <maiara.fonseca.ferreira@escola.pr.gov.br>
2025-09-16 10:19:52 -0300

    16/09

--- Perguntas.js
+++ Perguntas.js
@@ -5,58 +5,69 @@ criaCartao(
 )
 
 criaCartao(
-    'programacao',
-1) Quantos anos demorou para construir a torre de Paris ?,
-'R: 2 anos, 2 meses e 5 dias.'
+    'História',
+    'Quantos anos demorou para construir a torre de Paris?',
+    '2 anos, 2 meses e 5 dias'
+)
 
 criaCartao(
-2)De onde é a invenção do chuveiro elétrico ?,
-'R:Brasil'
+    'brasileira',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil'
+)
 
 criaCartao(
-3)Quantas casas decimais tem o número pi ?,
-'R:Infinitas'
+    'matematica',
+    'Quantas casas decimais tem o número pi ?',
+    'Infinitas'
+)
 
 criaCartao(
-4)Qual a montanha mais alta do Brasil ?,
-'R:Pico da Neblina'
+    'geografia',
+    'Qual a montanha mais alta do Brasil ?',
+    'Pico da Neblina'
+)
 
 criaCartao(
-5)Qual a velocidade da luz ?,
-'R: 299 792 458 metros por segundo (m/s)'
+    'matematica',
+    'Qual a velocidade da luz ?',
+    '299 792 458 metros por segundo (m/s)'
+)
 
 criaCartao(
-6)Quem é o autor de “O Príncipe”?,
-'R:Maquiavel'
+    'português',
+    'Quem é o autor de “O Príncipe”?,',
+    'Maquiavel'
+)
 
 criaCartao(
-7)Quais as maiores pandemias da história ?,
-'R:Peste negra e covid-19'
+    'história',
+    'Quais as maiores pandemias da história ?,',
+    'Peste negra e covid-19'
+)
 
 criaCartao(
-8)Quantos braços tem um polvo ?,
-'R:8'
+    'ciências',
+    'Quantos braços tem um polvo ?',
+    '8'
+)
 
 criaCartao(
-9)Quanto tempo o vidro demora para se decompor ?,
-'R:tempo indeterminado'
+    'química',
+    'Quanto tempo o vidro demora para se decompor ?',
+    'tempo indeterminado'
+)
 
 criaCartao(
-10)Quem inventou a lâmpada ?,
-'R: Thomas Edison'
-
-criaCartao(
-11)Quem foi a primeira pessoa a viajar no Espaço ?,
-'R:Yuri Gagarin'
-
-criaCartao(
-12)A que temperatura a água ferve ?,
-'R:100 ºC'
+    'fíica',
+    'Quem inventou a lâmpada ?',
+    'Thomas Edison'
+)
 
     
 
 
-)
+
 
 
 
